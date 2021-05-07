95bd6e4cf11aea5fd6e65455c072992a72392581
Hrishikesh Mohan <50671806+hkm7@users.noreply.github.com>
2021-05-07 19:48:02 +0530

    Backup 1

---
+++ __/firebase/init.js
@@ -0,0 +1,11 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyArxhYvYlsV15dnBoXTYw_HtW5cnCXstVY",
+  "appId": "1:539797122373:web:f978102de145da98f0e22c",
+  "authDomain": "iedc-cce.firebaseapp.com",
+  "databaseURL": "https://iedc-cce.firebaseio.com",
+  "measurementId": "G-XGBENYEWSP",
+  "messagingSenderId": "539797122373",
+  "projectId": "iedc-cce",
+  "storageBucket": "iedc-cce.appspot.com"
+});
